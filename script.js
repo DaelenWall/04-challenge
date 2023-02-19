b33bcf530bc8665fa706cdb0e66bda63188fe6f4
Daelen Wall <daelenwall@Vanessas-MacBook-Air.local>
2023-02-19 13:54:09 -0500

    Finished question array, created 'render' function for the questions, created 'compare' function to compare user choice with answer.

--- script.js
+++ script.js
@@ -10,34 +10,34 @@ var questions = [
     {
         question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
         choices: ["Variables", "Recorders", "Arrays", "Strings"],
-        correctAnswer: "Arrays"
+        correctAnswer: "Strings"
     },
     {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: ""
+        question: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?",
+        choices: ["Syntax", "JSON", "Scope", "Output"],
+        correctAnswer: "Syntax"
     },
     {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: ""
+        question: "In JavaScript, what element is used to store and manipulate text usually in multiples?",
+        choices: ["Arrays", "Function", "Strings", "Variables"],
+        correctAnswer: "Strings"
     },
     {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: ""
+        question: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+        choices: ["Restriction", "Output Level", "Range", "Scope"],
+        correctAnswer: "Scope"
     },
     {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: ""
+        question: "JavaScript does NOT have this function built-in, which is commonly used to print or display data in other languages.",
+        choices: ["Show", "Display", "Speak", "Output"],
+        correctAnswer: "Output"
     }
 ];
 
-var sco = 0;
+var scoreSet = 0;
 var qDisplay = 0;
 var intervalTest = 0;
-var timeMinus = 10;
+var yikes = 10;
 var timeLeft = 50;
 
 
@@ -55,4 +55,41 @@ startBtn.addEventListener("click", function(){
     }
     render(qDisplay);
 });
+
+// render questions function
+function render(qDisplay) {
+    // set to 0
+    score.innerHTML="";
+    ulTest.innerHTML="";
+    // make the questions appear one at a time
+    for (var i = 0; i < questions.length; i++){
+        var cueQuestion = questions[qDisplay].question;
+        var userChoice = questions[qDisplay].choices;
+        score.textContent = cueQuestion;
+    }
+    // make the choices appear to match the question
+        userChoice.forEach(function (newItem){
+            var liNew = document.createElement("li");
+                liNew.textContent = newItem;
+                score.appendChild(ulTest);
+                ulTest.appendChild(liNew);
+                liNew.addEventListener("click", (compare));
+            })
+}
+
+// if correctAnswer is chosen by user = correct
+function compare(event) {
+    var bananas = event.target;
+    if (bananas.matches("li")) {
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "createDiv");
+            if (bananas.textContent == questions   [qDisplay].correctAnswer) {
+                scoreSet++;
+                newDiv.textContent = "Correct!"
+            } else {
+                timeLeft = timeLeft - yikes;
+                newDiv.textContent = "Incorrect... The correct answer was: " + questions[qDisplay].correctAnswer;
+            }
+    }
+}
 
