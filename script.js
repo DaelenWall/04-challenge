9a1ffc5f7d98f990f05eb70e15ace4580de85516
Daelen Wall <daelenwall@Vanessas-MacBook-Air.local>
2023-02-19 12:22:41 -0500

    Created question array, added globals, and added startBtn function with working timer.

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-// script.js
+//html pulls
+var timer = document.querySelector("#timer");
+var score = document.querySelector("#score");
+var startBtn = document.querySelector("#startBtn");
+var ulTest =document.createElement("ul");
+
+
+//global variables
+var questions = [
+    {
+        question: "In JavaScript, what element is used to store and manipulate text, usually in multiples?",
+        choices: ["Variables", "Recorders", "Arrays", "Strings"],
+        correctAnswer: "Arrays"
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        correctAnswer: ""
+    }
+];
+
+var sco = 0;
+var qDisplay = 0;
+var intervalTest = 0;
+var timeMinus = 10;
+var timeLeft = 50;
+
+
+// start button function
+startBtn.addEventListener("click", function(){
+    if (intervalTest == 0) {
+        intervalTest = setInterval(function(){
+            timeLeft--;
+            timer.textContent = "Time: " + timeLeft;
+                if (timeLeft <= 0) {
+                    clearInterval(intervalTest);
+                    timer.textContent = "No Time Left!";
+                }
+        }, 1000);
+    }
+    render(qDisplay);
+});
+
